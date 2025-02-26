3ff1b74e753805cc3d709b5247db0c84ac8ac6f2
nevena-m <nevenamesar96@gmail.com>
2025-02-26 19:25:10 +0100

    Fake Store - Client - handle the server error on fetching products

--- client/src/containers/ProductListPage/index.tsx
+++ client/src/containers/ProductListPage/index.tsx
@@ -1,39 +1,55 @@
-import { useEffect } from "react";
-import { useLazyGetProductsQuery } from "../../api/productsApi";
-import Spinner from "../../commonComponents/Spinner";
-import ProductCard from "../ProductCard";
-import SearchProducts from "../SearchProducts";
-import { ProductsFilter } from "../../types/products";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/query"
+import { useEffect, useMemo } from "react"
+import { useLazyGetProductsQuery } from "../../api/productsApi"
+import Spinner from "../../commonComponents/Spinner"
+import { MessageDescription } from "../../types/errors"
+import { ProductsFilter } from "../../types/products"
+import ProductCard from "../ProductCard"
+import SearchProducts from "../SearchProducts"
 
 export default function ProductListPage() {
+	const [triggerGetProducts, getProductsResponse] = useLazyGetProductsQuery()
 
-    const [triggerGetProducts, getProductsResponse] = useLazyGetProductsQuery();
+	useEffect(() => {
+		// on mount
+		triggerGetProducts({})
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
 
-    useEffect(() => {
-        // on mount
-        triggerGetProducts({});
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+	const handleOnSearch = (filter: ProductsFilter) => {
+		triggerGetProducts(filter)
+	}
 
-    const handleOnSearch = (filter: ProductsFilter) => {
-        triggerGetProducts(filter);
-    }
+	const errorMessage = useMemo(() => {
+		const errorDesc = (
+			(getProductsResponse?.error as FetchBaseQueryError)
+				?.data as MessageDescription
+		)?.description
 
-    return (
-        <div className="flex p-8 flex-col">
-            <div className="flex flex-col">
-                <SearchProducts onSearch={handleOnSearch} />
-                {getProductsResponse?.data?.totalCount && <div className="flex m-auto mt-2">Total products: {getProductsResponse?.data?.totalCount}</div>}
-            </div>
-            <div className="flex flex-wrap gap-8 mt-4 ">
-                {getProductsResponse.isLoading && <Spinner />}
-                {
-                    getProductsResponse
-                        ?.data
-                        ?.productList
-                        ?.map((product) => <ProductCard key={product.id} product={product} />)
-                }
-            </div>
-        </div>
-    )
+		return errorDesc
+	}, [getProductsResponse?.error])
+
+	return (
+		<div className="flex p-8 flex-col">
+			{errorMessage && (
+				<div className="flex bg-red-300 p-3 text-center mb-3">
+					{errorMessage}
+				</div>
+			)}
+			<div className="flex flex-col">
+				<SearchProducts onSearch={handleOnSearch} />
+				{getProductsResponse?.data?.totalCount && (
+					<div className="flex m-auto mt-2">
+						Total products: {getProductsResponse?.data?.totalCount}
+					</div>
+				)}
+			</div>
+			<div className="flex flex-wrap gap-8 mt-4 ">
+				{getProductsResponse.isLoading && <Spinner />}
+				{getProductsResponse?.data?.productList?.map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+			</div>
+		</div>
+	)
 }
--- client/src/containers/SearchProducts/index.tsx
+++ client/src/containers/SearchProducts/index.tsx
@@ -1,43 +1,52 @@
-import { useMemo, useState } from "react";
-import { useGetProductCategoriesQuery } from "../../api/productsApi";
-import Select, { SelectOption } from "../../commonComponents/Select";
-import { ProductsFilter } from "../../types/products";
+import { useMemo, useState } from "react"
+import { useGetProductCategoriesQuery } from "../../api/productsApi"
+import Select, { SelectOption } from "../../commonComponents/Select"
+import { ProductsFilter } from "../../types/products"
+
+const ALL_CATEGORIES: string = "ALL_CATEGORIES" as const
 
 interface SearchProductsProps {
-    onSearch: (filter: ProductsFilter) => void;
+	onSearch: (filter: ProductsFilter) => void
 }
 
+const defaultCategoryOptions = [
+	{ label: "All Categories", value: ALL_CATEGORIES },
+]
+
 export default function SearchProducts({
-    onSearch
+	onSearch,
 }: SearchProductsProps) {
+	const [currentValue, setCurrentValue] = useState(null)
 
-    const [currentValue, setCurrentValue] = useState(null);
-    
-    const {
-        isLoading,
-        data: categories,
-        isError,
-    } = useGetProductCategoriesQuery();
+	const {
+		isLoading,
+		data: categories,
+		isError,
+	} = useGetProductCategoriesQuery()
 
-    const handleSelectCategory = (value) => {
-        setCurrentValue(value);
-        onSearch({
-            categoryName: value
-        });
-    }
+	const handleSelectCategory = (value) => {
+		const newValue = value === ALL_CATEGORIES ? null : value
+		setCurrentValue(newValue)
+		onSearch({
+			categoryName: newValue,
+		})
+	}
 
-    const categorySelectOptions = useMemo<SelectOption[]>(() => {
-        return Array.isArray(categories) ? categories.map(name => ({ label: name, value: name })) : []
-    }, [categories]);
+	const categorySelectOptions = useMemo<SelectOption[]>(() => {
+		return Array.isArray(categories)
+			? defaultCategoryOptions
+					.slice()
+					.concat(categories.map((name) => ({ label: name, value: name })))
+			: defaultCategoryOptions
+	}, [categories])
 
-    return (
-        <Select
-            onChange={handleSelectCategory}
-            isError={isError}
-            isLoading={isLoading}
-            options={categorySelectOptions}
-            selectedValue={currentValue}
-        />
-
-    )
+	return (
+		<Select
+			onChange={handleSelectCategory}
+			isError={isError}
+			isLoading={isLoading}
+			options={categorySelectOptions}
+			selectedValue={currentValue}
+		/>
+	)
 }
